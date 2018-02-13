a98af2775db1bb49e6e3a89010503747cf24697d
xy-IMAW <952137172@qq.com>
2018-02-13 21:59:18 +0800

    2018.02.13
    登录页面修改（按钮字体显示bug）

--- ExperimentTen/Login.aspx.designer.cs
+++ ExperimentTen/Login.aspx.designer.cs
@@ -49,12 +49,12 @@ namespace WHMS {
         protected global::System.Web.UI.WebControls.Button btnLogin;
         
         /// <summary>
-        /// btnreset 控件。
+        /// btnReset 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnreset;
+        protected global::System.Web.UI.WebControls.Button btnReset;
     }
 }
